4d68b91bc50a3d23ad554b8228950975f707a16f
Neyba <zubankovaanastasia26@gmail.com>
2023-06-29 12:10:55 +0500

    Добавление шаблона подтверждения регистрации

--- vue/src/router/routes.js
+++ vue/src/router/routes.js
@@ -1,6 +1,8 @@
 //import HelloWorld from '../components/HelloWorld.vue'
 import Register from '../components/Register.vue'
 import Login from '../components/Login.vue'
+import confirmRegister from '../components/confirmRegister.vue'
+import endRegister from '../components/endRegister.vue'
 
 export const routes = [
     {
@@ -18,5 +20,14 @@ export const routes = [
         path: '/login',
         component: Login
     },
-
+    {
+        name: "ConfReg",
+        path: '/confirm-registration',
+        component: confirmRegister
+    },
+    {
+        name: "EndReg",
+        path: '/end-registration',
+        component: endRegister
+    },
 ]
