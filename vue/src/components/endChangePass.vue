172a59cb2b2c4325f1971d91566b2f02aa1568b1
Neyba <zubankovaanastasia26@gmail.com>
2023-06-30 11:41:38 +0500

    Добавление шаблона изменения пароля

---
+++ vue/src/components/endChangePass.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="card">
+        <h1 class="card-header">Смените пароль</h1>
+        <div class="card-body">
+            <form class="general-block" @submit.prevent="LoginData">
+                <div><input class="user-passw" type="password" placeholder="Новый пароль*"/></div>
+                <div><input class="user-passw" type="password" placeholder="Подтверждение пароля*"/></div>
+                <button type="submit" class="button">Поменять пароль</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<style src="../components/compStyle/endChangePass.css" scoped>
+</style>
+
+<script>
+import axios from 'axios';
+export default {
+    data () {
+        return {
+            result: {},
+
+            email: '',
+            password: ''
+
+        }
+    },
+
+    methods: {
+        LoginData()
+        {
+            axios.post("http://app/api/login", this.student)
+                .then(
+                    ({data})=>{
+                        console.log(data);
+                        try {
+                            if (data.status === true) {
+                                alert("Login Successfully");
+                                this.$router.push({ name: 'HelloWorld' })
+                            } else {
+                                alert("Login failed")
+                            }
+
+                        } catch (err) {
+                            alert("Error, please try again");
+                        }
+                    }
+                )
+        }
+    }
+}
+</script>
--- vue/src/router/routes.js
+++ vue/src/router/routes.js
@@ -3,6 +3,7 @@ import Register from '../components/Register.vue'
 import Login from '../components/Login.vue'
 import Recovery from '../components/PassRecovery.vue'
 import Confirm from '../components/confirmPassRecovery.vue'
+import Change from '../components/endChangePass.vue'
 
 export const routes = [
     {
@@ -31,6 +32,12 @@ export const routes = [
         name: "Confirm",
         path: '/confirm-pass',
         component: Confirm
+    },
+
+    {
+        name: "Change",
+        path: '/end-change-pass',
+        component: Change
     },
 
 ]
