9072d5089d40a0a866db2913d8f84087280aab09
Анастасия <zubankovaanastasia26@gmail.com>
2023-07-19 15:48:08 +0500

    Добавление демо данных

--- database/migrations/2023_07_18_110108_create_test_data_table.php
+++ database/migrations/2023_07_18_110108_create_test_data_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,13 +12,13 @@ return new class extends Migration
     {
         Schema::create('test_data', function (Blueprint $table) {
             $table->id();
+            $table->string('campaign');
+            $table->string('ad');
+            $table->string('keywords');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('test_data');
--- database/seeders/TestDataSeeder.php
+++ database/seeders/TestDataSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class TestDataSeeder extends Seeder
 {
@@ -12,6 +14,43 @@ class TestDataSeeder extends Seeder
      */
     public function run(): void
     {
-        //
-    }
+        $campaigns = [
+            'Установка кондиционеров',
+            'Плюшевые медведи',
+        ];
+        $ads = [
+            'Установка кондиционеров МСК',
+            'Плюшевые медведи МСК',
+        ];
+        $keywords = [
+            [
+                'установка кондиционера',
+                'установка кондиционера в москве',
+                'купить кондиционер +с установкой недорого',
+            ],
+            [
+                'плюшевая игрушка медведь',
+                'игрушка большой плюшевый медведь',
+                'плюшевый мишка купить'
+            ],
+        ];
+
+        $i = 0;
+        foreach ($campaigns as $camp_name) {
+            $campaign[$camp_name] = $ads[$i++];
+        }
+            $i = 0;
+            foreach ($campaign as $key => $ad) {
+                $value = $keywords[$i++];
+                foreach ($value as $key_value => $keyword) {
+                        DB::table('test_data')->insert([
+                            'campaign' => $key,
+                            'ad' => $ad,
+                            'keywords' => $keyword,
+                            'created_at' => now(),
+                            'updated_at' => now(),
+                        ]);
+                    }
+                }
+            }
 }
