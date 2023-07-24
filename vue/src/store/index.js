fb72e63885f6ca674e2ae4114a9942eb368679e9
alex89 <alex.dya4encko89@yandex.ru>
2023-07-24 20:19:44 +0500

    vuex подтверждение пароля

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -2,14 +2,27 @@ import {createStore} from "vuex";
 import {Authtorisation} from '../store/LogModul.js'
 export default createStore({
     state: ()=>({
+        seconds:60,
+        minuts:1,
+     }),
+     mutations:{
+         changeTime(state){
+             if(state.minuts > 0){
+                 state.minuts-=1
+             }
+             if(state.seconds > 0){
+                 state.seconds-= 1;
+             }
+        },
+     },
+     actions:{
+         changeTimeAsync({commit}){
+             setInterval(()=>{
+                 commit('changeTime')
+             },1000);
+         },
+     },
 
-    }),
-    getters:{
-    },
-    mutations:{
-    },
-    actions:{
-    },
     modules:{
         Auth:Authtorisation,
     }
