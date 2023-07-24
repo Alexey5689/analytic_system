f1bb7e12de11dbb1e6ec89375ebd0b01f29f5ca1
Анастасия <zubankovaanastasia26@gmail.com>
2023-07-24 17:50:28 +0500

    рефакторинг регистрации

--- vue/src/components/confirmRegister.vue
+++ vue/src/components/confirmRegister.vue
@@ -1,20 +1,21 @@
 <template>
+    <div class="container">
     <div class="card">
-
         <h1 class="card-header">Подтверждение регистрации</h1>
         <div class="ruls">
             {{ state.response }}
         </div>
         <h3>Спасибо за регистрацию!</h3>
         <div class="card-body">
-            <p>На вашу почту была отправлена ссылка подтверждения регистрации. Перейдите по ссылке в письме для подтверждения</p>
+            <p class="main-text">На вашу почту была отправлена ссылка подтверждения регистрации. Перейдите по ссылке в письме для подтверждения</p>
             <div class="bottom-block-register">
                 <p>Не пришло письмо?</p>
                 <p v-if="seconds>0" class=" ">Отправить повторно</p>
                 <p v-else class="resend-p" @click="repeatReqest" >Отправить повторно</p>
             </div>
-            <p>{{minuts<1?'00':'0'+minuts}}:{{minuts===1?'00':seconds<10?'0'+seconds:seconds}}</p>
+            <p class="time">{{minuts<1?'00':'0'+minuts}}:{{minuts===1?'00':seconds<10?'0'+seconds:seconds}}</p>
         </div>
+    </div>
     </div>
 </template>
 <script>
