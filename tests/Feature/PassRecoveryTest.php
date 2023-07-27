144fd4387b0089fe4e85923a92d5fa6efa348223
Анастасия <zubankovaanastasia26@gmail.com>
2023-07-27 17:20:44 +0500

    редактирование feature теста

--- tests/Feature/PassRecoveryTest.php
+++ tests/Feature/PassRecoveryTest.php
@@ -2,12 +2,12 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class PassRecoveryTest extends TestCase
 {
+    use DatabaseMigrations;
     /**
      * A basic feature test example.
      */
@@ -17,4 +17,54 @@ class PassRecoveryTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_function_pass_recovery(): void
+    {
+        $this->postJson('/api/register',
+            [
+                'name' => 'Qwerty',
+                'tel' => '89096096127',
+                'email' => 'next@mail.ru',
+                'password' => 'qwerty12',
+            ]);
+      $response = $this->post('/api/forget-password',
+            [
+                'email' => 'next@mail.ru',
+            ]);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(
+                [
+                    'message',
+                    'status',
+                ]);
+    }
+
+    public function test_function_reset_pass_recovery(): void
+    {
+        $this->postJson('/api/register',
+            [
+                'name' => 'Qwerty',
+                'tel' => '89096096127',
+                'email' => 'next@mail.ru',
+                'password' => 'qwerty12',
+            ]);
+        $response = $this->post('/api/reset',
+            [
+                'email' => 'next@mail.ru',
+                'password' => 'qweQWE1!',
+                'password_confirmation' => 'qweQWE1!',
+                'token' => '330261a8a6c67c0c6fce44eeff9627cda43af6513080a9daaccd6813d3dd9bb0'
+            ]);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(
+                [
+                    'message',
+                    'status',
+                ]);
+    }
+
+
+
 }
