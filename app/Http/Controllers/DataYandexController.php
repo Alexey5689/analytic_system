3f029cf90d49aca93369b2a1dca3b76d61f2b597
Анастасия <zubankovaanastasia26@gmail.com>
2023-07-28 18:22:15 +0500

    рефакторинг контроллера

--- app/Http/Controllers/DataYandexController.php
+++ app/Http/Controllers/DataYandexController.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\GetCampaignFromYandexBot;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class DataYandexController extends Controller
 {
-    public function get_campaign()
+    public function get_campaign(Request $request)
     {
-        $response = Http::get((config('api.url').':'.(config('api.port').'/api/format_campaigns_data')));
-        return $response->json();
+        $response = Http::get(config('api.url').':'.(config('api.port').'/api/format_campaigns_data'))->json();
+        foreach($response as $campaign) {
+            $conversion_formula = $campaign['impressions']/$campaign['clicks']*100;
+            $campaign_data[] = [
+                'campaign_name' => $campaign['name'],
+                'visits' => $campaign['impressions'],
+                'applications' => $campaign['clicks'],
+                'conversion' => round($conversion_formula, 2),
+            ];
+        }
     }
-
 }
