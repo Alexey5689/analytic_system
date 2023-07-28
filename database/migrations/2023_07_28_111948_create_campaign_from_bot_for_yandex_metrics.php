b0b6305a16fa2f5e255765aa914ea81a8f73013a
Анастасия <zubankovaanastasia26@gmail.com>
2023-07-28 16:41:26 +0500

    рефакторинг миграций

---
+++ database/migrations/2023_07_28_111948_create_campaign_from_bot_for_yandex_metrics.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('campaign_from_bot_for_yandex_metrics', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('campaign_from_bot_for_yandex_metrics');
+    }
+};
