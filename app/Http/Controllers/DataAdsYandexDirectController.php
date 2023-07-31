1bc5cd984253fffdc88fe6f0e668402f8862d694
Анастасия <zubankovaanastasia26@gmail.com>
2023-07-31 20:15:59 +0500

    добавление данных с директа для объявлений,рефакторинг миграций и роутов

---
+++ app/Http/Controllers/DataAdsYandexDirectController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AdsFromBotForYandexMetrics;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Http;
+
+
+class DataAdsYandexDirectController extends Controller
+{
+    public function get_ads()
+    {
+        $response = Http::get(config('api.url') . ':' . (config('api.port') . '/api/format_ad_data'))->json();
+        foreach($response as $ad_group) {
+            $ad_data[] = [
+                'ad_name' => $ad_group['title'],
+                'ads_list_id' => $ad_group['ads_list_id'],
+                'impressions' => rand(),
+                'clicks' => rand(),
+                'daily_budget' => rand(),
+            ];
+        }
+        return $ad_data;
+    }
+
+    public function __invoke(){
+        AdsFromBotForYandexMetrics::upsert($this->get_ads(),['id'], [
+            'ad_name',
+            'ads_list_id',
+            'impressions',
+            'clicks',
+            'daily_budget',
+        ]);
+    }
+
+    public function send_api_ads() {
+        if($this->get_ads() !== null ) {
+            return AdsFromBotForYandexMetrics::all()->toJson();
+        }
+        else {
+            return response()->json(
+                [
+                    'status' => false,
+                    'message' => 'Нет данных'
+                ]
+            );
+        }
+    }
+}
