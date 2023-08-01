1c2fbef7359a8ef18659c093045d832814ec825f
Анастасия <zubankovaanastasia26@gmail.com>
2023-08-01 15:06:58 +0500

    добавление демо данных и миграций

---
+++ database/seeders/TestDataCampaignSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+
+class TestDataCampaignSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $campaigns = [
+            'Установка кондиционеров',
+            'Плюшевые медведи',
+        ];
+
+        foreach ($campaigns as $campaign) {
+            DB::table('test_data_campaign')->insert([
+                'campaign_name' => $campaign,
+                'impressions' => rand(),
+                'clicks' =>  rand(),
+                'daily_budget' => rand(),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
